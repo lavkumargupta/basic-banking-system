c6d0dfc187cd1c7ae64e6c46b8d3e872e98c1bd3
lavkumargupta <lavkumargupta2002@gmail.com>
2022-09-10 13:56:02 +0530

    Added index file and composer.json

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+	// $servername = 'vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	// $user = 'wdfa08zvh6lz2hoj';
+	// $pass = 'zudcu35z3zeb5puz';
+	// $dbname = 'r0yol1itzflzm42x';
+
+	$servername = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$dbname = 'spark';
+
+	$conn = mysqli_connect($servername,$user,$pass,$dbname);
+
+	if(!$conn){
+		die("Could Not Connect to the database".mysqli_connect_error());
+	}
+
+?>
